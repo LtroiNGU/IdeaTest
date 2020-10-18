c2cd6910e59fee1fdc4fb7b7a6f7b96ae1af1a9b
oliver <olivernguppp@outlook.com>
2020-10-18 12:06:15 +0800

    test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test git for idea");
+        System.out.println("test git for ideassssssssssssssssssssssssssssssssss");
     }
 }
